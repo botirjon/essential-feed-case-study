3ea28deae4eefb11fc02514a22cb4161eec4e147
botirjon <botirjon.nasridinov@gmail.com>
2024-12-14 17:31:59 +0500

    CoreDataFeedStore specs

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,61 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Botirjon Nasridinov on 14/12/24.
+//
+
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    
+    func test_retreive_deliversEmptyOnEmptyCache() {
+            
+    }
+
+    func test_retreive_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+
+    func test_retreive_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+
+    func test_retreive_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+
+    func test_storeSideEffects_runSerially() {
+        
+    }
+
+    
+}
