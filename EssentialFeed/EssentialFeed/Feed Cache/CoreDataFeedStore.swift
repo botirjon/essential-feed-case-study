18fb13b0ace74c424ebf9b94599ff56a726004ba
botirjon <botirjon.nasridinov@gmail.com>
2024-12-14 23:33:40 +0500

    CoreDataFeedStore.insert() overrides previously inserted cache values

--- EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -31,7 +31,7 @@ public final class CoreDataFeedStore: FeedStore {
         let context = self.context
         context.perform {
             do {
-                let managedCache = ManagedCache(context: context)
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
                 managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
                 
@@ -51,6 +51,7 @@ public final class CoreDataFeedStore: FeedStore {
                     entityName: ManagedCache.entity()
                         .name!)
                 request.returnsObjectsAsFaults = false
+                
                 if let cache = try ManagedCache.find(in: context) {
                     completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
                 } else {
@@ -104,6 +105,11 @@ class ManagedCache: NSManagedObject {
         let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
         request.returnsObjectsAsFaults = false
         return try context.fetch(request).first
+    }
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
     }
     
     var localFeed: [LocalFeedImage] {
