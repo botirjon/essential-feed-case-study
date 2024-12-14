45d452d7d4db5f8a7ebcda014cd9a60d8535e6a8
botirjon <botirjon.nasridinov@gmail.com>
2024-12-14 22:30:41 +0500

    Add `CoreDataFeedStore` model

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -6,19 +6,47 @@
 //
 
 import XCTest
+import EssentialFeed
+
+class CoreDataFeedStore: FeedStore {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+
+    func insert(
+        _ feed: [EssentialFeed.LocalFeedImage],
+        timestamp: Date,
+        completion: @escaping InsertionCompletion
+    ) {
+        
+    }
+
+    func retreive(completion: @escaping RetreiveCompletion) {
+        completion(.empty)
+    }
+
+    
+}
+
 
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_retreive_deliversEmptyOnEmptyCache() {
-            
+        let sut = makeSUT()
+        
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
 
     func test_retreive_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
 
     func test_retreive_deliversFoundValuesOnNonEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
     }
 
     func test_retreive_hasNoSideEffectsOnNonEmptyCache() {
@@ -57,5 +85,11 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
         
     }
 
+    // MARK: - Helpers
     
+    func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
 }
