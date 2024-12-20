7af5e940830a0aa3d8b834f03a91401f40c4d45f
botirjon <botirjon.nasridinov@gmail.com>
2024-12-20 15:39:37 +0500

    Extract `FeedImageCell` creation/configuration and image loading logic with `FeedImageDataLoader` to the new `FeedImageCellController`

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import EssentialFeed
 
+
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     private var refreshController: FeedRefreshViewController?
     private var imageLoader: FeedImageDataLoader?
@@ -16,7 +17,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
             tableView.reloadData()
         }
     }
-    private var tasks = [IndexPath: FeedImageDataLoaderTask]()
+    private var cellControllers = [IndexPath: FeedImageCellController]()
     private var viewAppeared: Bool = false
     
     public convenience init(
@@ -51,30 +52,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellModel = tableModel[indexPath.row]
-        let cell = FeedImageCell()
-        cell.locationContainer.isHidden = cellModel.location == nil
-        cell.locationLabel.text = cellModel.location
-        cell.descriptionLabel.text = cellModel.description
-        cell.feedImageContainer.startShimmering()
-        cell.feedImageView.image = nil
-        cell.retryButton.isHidden = true
-        
-        let loadImage = { [weak self, weak cell] in
-            guard let self = self else { return }
-            self.tasks[indexPath] = self.imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
-                let data = try? result.get()
-                let image = data.map(UIImage.init) ?? nil
-                cell?.feedImageView.image = image
-                cell?.retryButton.isHidden = (image != nil)
-                cell?.feedImageContainer.stopShimmering()
-                
-            }
-        }
-        
-        cell.onRetry = loadImage
-        loadImage()
-        return cell
+        return cellController(forRowAt: indexPath).view()
     }
     
     public override func tableView(
@@ -82,8 +60,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
         didEndDisplaying cell: UITableViewCell,
         forRowAt indexPath: IndexPath
     ) {
-        tasks[indexPath]?.cancel()
-        tasks[indexPath] = nil
+        removeCellController(forRowAt: indexPath)
     }
     
     public func tableView(
@@ -91,8 +68,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
         prefetchRowsAt indexPaths: [IndexPath]
     ) {
         indexPaths.forEach {
-            let cellModel = tableModel[$0.row]
-            tasks[$0] = imageLoader?.loadImageData(from: cellModel.url) { _ in }
+            cellController(forRowAt: $0).preload()
         }
     }
     
@@ -100,13 +76,20 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
         _ tableView: UITableView,
         cancelPrefetchingForRowsAt indexPaths: [IndexPath]
     ) {
-        indexPaths.forEach {
-            cancelTask(forRowAt: $0)
-        }
+        indexPaths.forEach(removeCellController)
     }
     
-    private func cancelTask(forRowAt indexPath: IndexPath) {
-        tasks[indexPath]?.cancel()
-        tasks[indexPath] = nil
+    private func removeCellController(forRowAt indexPath: IndexPath) {
+        cellControllers[indexPath] = nil
+    }
+    
+    private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
+        let cellModel = tableModel[indexPath.row]
+        let cellController = FeedImageCellController(
+            model: cellModel,
+            imageLoader: imageLoader!
+        )
+        cellControllers[indexPath] = cellController
+        return cellController
     }
 }
