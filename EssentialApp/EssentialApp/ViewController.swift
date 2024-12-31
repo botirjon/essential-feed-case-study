382398347093a0532ee8c790faf3c6dcf1653fcc
botirjon <botirjon.nasridinov@gmail.com>
2024-12-31 22:34:48 +0500

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Botirjon Nasridinov on 31/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
