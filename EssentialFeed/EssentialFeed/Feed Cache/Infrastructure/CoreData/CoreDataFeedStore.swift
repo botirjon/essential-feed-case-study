8b6693e215c6f8fc268197055711a875bf4a65cc
botirjon <botirjon.nasridinov@gmail.com>
2024-12-31 21:41:47 +0500

    Move CoreDataFeedStore implementation of the FeedStore protocol to separate file

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -8,7 +8,7 @@
 import Foundation
 import CoreData
 
-public final class CoreDataFeedStore: FeedStore {
+public final class CoreDataFeedStore {
     
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
@@ -17,45 +17,6 @@ public final class CoreDataFeedStore: FeedStore {
         container = try NSPersistentContainer
             .load(modelName: "FeedStore", url: storeURL, in: bundle)
         context = container.newBackgroundContext()
-    }
-    
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        perform { context in
-            completion(Result {
-                try ManagedCache
-                    .find(in: context)
-                    .map(context.delete)
-                    .map(context.save)
-                return ()
-            })
-        }
-    }
-    
-    public func insert(
-        _ feed: [EssentialFeed.LocalFeedImage],
-        timestamp: Date,
-        completion: @escaping InsertionCompletion
-    ) {
-        perform { context in
-            completion(Result {
-                let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                managedCache.timestamp = timestamp
-                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
-                
-                try context.save()
-                return ()
-            })
-        }
-    }
-    
-    public func retreive(completion: @escaping FeedStore.RetreiveCompletion) {
-        perform { context in
-            completion(Result {
-                try ManagedCache.find(in: context).map {
-                    return CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
-                }
-            })
-        }
     }
     
     func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
