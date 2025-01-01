2917a1085117f885d3262dca0392417cff5c9b6c
botirjon <botirjon.nasridinov@gmail.com>
2025-01-02 01:12:32 +0500

    `FeedImageDataLoaderCachingDecorator` init does not load image data

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCachingDecoratorTests.swift
@@ -0,0 +1,78 @@
+//
+//  FeedImageDataLoaderCachingDecoratorTests.swift
+//  EssentialApp
+//
+//  Created by Botirjon Nasridinov on 02/01/25.
+//
+
+
+import XCTest
+import EssentialFeed
+
+private final class FeedImageDataLoaderCachingDecorator: FeedImageDataLoader {
+    
+    private let decoratee: FeedImageDataLoader
+    
+    init(decoratee: FeedImageDataLoader) {
+        self.decoratee = decoratee
+    }
+    
+    private class Task: EssentialFeed.FeedImageDataLoaderTask {
+        func cancel() {
+            
+        }
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> any EssentialFeed.FeedImageDataLoaderTask {
+        return Task()
+    }
+}
+
+final class FeedImageDataLoaderCachingDecoratorTests: XCTestCase {
+    
+    func test_init_doesNotLoadImageData() {
+        let loader = LoaderSpy()
+        _ = FeedImageDataLoaderCachingDecorator(decoratee: loader)
+        
+        XCTAssertTrue(loader.loadedURLs.isEmpty)
+    }
+    
+    // MARK: - Helpers
+    
+    private class LoaderSpy: FeedImageDataLoader {
+        
+        private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedURLs: [URL] {
+            messages.map { $0.url }
+        }
+        
+        private(set) var cancelledURLs = [URL]()
+        
+        private class Task: FeedImageDataLoaderTask {
+            
+            let callback: () -> Void
+            
+            init(callback: @escaping () -> Void) {
+                self.callback = callback
+            }
+            
+            func cancel() {
+                callback()
+            }
+        }
+        
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> any EssentialFeed.FeedImageDataLoaderTask {
+            messages.append((url, completion))
+            return Task { [weak self] in self?.cancelledURLs.append(url) }
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
+        }
+        
+        func complete(with data: Data, at index: Int = 0) {
+            messages[index].completion(.success(data))
+        }
+    }
+}
