b8e8ed42147afd139a0e46735304f7e9078720a8
botirjon <botirjon.nasridinov@gmail.com>
2025-01-02 00:32:36 +0500

    Extract `FeedLoader` test helper into a shared protocol extension to remove duplication

--- EssentialApp/EssentialAppTests/FeedLoaderCachingDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCachingDecoratorTests.swift
@@ -20,7 +20,7 @@ private final class FeedLoaderCachingDecorator: FeedLoader {
     }
 }
 
-final class FeedLoaderCachingDecoratorTests: XCTestCase {
+final class FeedLoaderCachingDecoratorTests: XCTestCase, FeedLoaderTestCase {
     
     func test_load_deliversFeedOnLoaderSuccess() {
         let feed = uniqueFeed()
@@ -35,34 +35,6 @@ final class FeedLoaderCachingDecoratorTests: XCTestCase {
         let sut = FeedLoaderCachingDecorator(decoratee: loader)
         
         expect(sut, toCompleteWith: .failure(anyNSError()))
-    }
-    
-    
-    private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #filePath, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
-        
-        sut.load { receivedResult in
-            switch (receivedResult, expectedResult) {
-                case let (.success(receivedFeed), .success(expectedFeed)):
-                    XCTAssertEqual(
-                        receivedFeed,
-                        expectedFeed,
-                        file: file,
-                        line: line
-                    )
-                    
-                case (.failure, .failure):
-                    break
-                    
-                default:
-                    XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            
-            
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
     }
 }
 
